df5cd87787d5983a524278798b7825332087c2d0
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-01-05 21:45:12 -0600

    Created a function to return props from note component. Added to App component

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "./notes";
+
+function createNote(note) {
+  return <Note key={note.key} title={note.title} content={note.content} />;
+}
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Note />
+      <div>{notes.map(createNote)}</div>
       <Footer />
     </div>
   );
